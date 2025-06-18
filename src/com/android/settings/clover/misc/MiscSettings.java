50a41112e37f24a6dcdf2d1d5515c068e05f23ad
Abhay Singh Gill <abhaygill017@gmail.com>
2025-06-18 11:50:21 +0000

    Settings: Allow user to provide keybox data [2/2]
    Change-Id: If4a0bce8e7ef07d7649e128b75c20de141993c87
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>

--- src/com/android/settings/clover/misc/MiscSettings.java
+++ src/com/android/settings/clover/misc/MiscSettings.java
@@ -1,18 +1,57 @@
 package com.android.settings.clover.misc;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+
+import androidx.preference.Preference;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.android.settings.preferences.KeyboxDataPreference;
+
 public class MiscSettings extends SettingsPreferenceFragment {
+
+    private static final String KEYBOX_DATA_KEY = "keybox_data_setting";
+    private ActivityResultLauncher<Intent> mKeyboxFilePickerLauncher;
+    private KeyboxDataPreference mKeyboxDataPreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.misc_settings);
+
+        mKeyboxFilePickerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    Uri uri = result.getData().getData();
+                    if (uri != null) { 
+                        Preference pref = findPreference(KEYBOX_DATA_KEY);
+                        if (pref instanceof KeyboxDataPreference) {
+                            ((KeyboxDataPreference) pref).handleFileSelected(uri);
+                        }
+                    }
+                }
+            }
+        );
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mKeyboxDataPreference = findPreference(KEYBOX_DATA_KEY);
+        if (mKeyboxDataPreference != null) {
+            mKeyboxDataPreference.setFilePickerLauncher(mKeyboxFilePickerLauncher);
+        }
     }
 
     @Override
